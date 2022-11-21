7c8215f97041f45a620dfb2c298bd6fd1fca1df0
jnetop <jnetopessoal@gmail.com>
2022-11-21 02:15:53 -0300

    feat: Adds aws_key_pair and aws_security_group resources

--- example1-creating-a-virtual-machine/main.tf
+++ example1-creating-a-virtual-machine/main.tf
@@ -17,7 +17,35 @@ resource "aws_instance" "app_server" {
   ami           = "ami-017fecd1353bcc96e"
   instance_type = "t2.micro"
   key_name = "iac-playground-example1"
+  vpc_security_group_ids = [aws_security_group.total_access.id]
   tags = {
     Name = "CreatingAVirtualMachineExample"
+  }
+}
+
+resource "aws_key_pair" "ssh_key" {
+  key_name = "iac-playground-example1"
+  public_key = file("iac-playground-example1.pub")
+}
+
+resource "aws_security_group" "total_access" {
+  name = "total_access"
+  description = "A permissive security group"
+  ingress {
+    cidr_blocks = [ "0.0.0.0/0" ]
+    ipv6_cidr_blocks = [ "::/0" ]
+    from_port = 0
+    to_port = 0
+    protocol = -1
+  }
+  egress {
+    cidr_blocks = [ "0.0.0.0/0" ]
+    ipv6_cidr_blocks = [ "::/0" ]
+    from_port = 0
+    to_port = 0
+    protocol = -1
+  }
+  tags = {
+    name = "total_access"
   }
 }
